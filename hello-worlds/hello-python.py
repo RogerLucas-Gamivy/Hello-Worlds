db9833b05dcacfa9956513be31314bac1455f296
Roger Lucas <gamivy.br@gmail.com>
2025-09-09 20:36:32 -0300

    v0.0.4 - Update hello-python.py
    Added how to do Python comments and a "Hello, World!" print.

---
+++ hello-worlds/hello-python.py
@@ -0,0 +1,8 @@
+# This is a Python line comment
+"""
+This is a Python docstring that isn't attached to a variable.
+It can be used as a block comment, as Python Interpreter just ignore it.
+Use it with caution, as it allocates memory, even if minimal.
+"""
+
+print("Hello, World!")
